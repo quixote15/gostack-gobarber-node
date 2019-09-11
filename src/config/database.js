35e6b06061e17b1754240d6bda46ae41af2e7d32
quixote15 <santos2tiago@gmail.com>
2019-09-11 14:19:07 -0300

    Refinish sequelize configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,16 @@
+
+/**
+ *  Para utilizar o postgres é necessário instalar o pg e pg-hstore
+ */
 module.exports = {
-  dialect: 'postgres',
+  dialect: 'postgres', // aceita mongo e mariadb
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
   define: {
-    timestamps: true,
-    underscored: true,
+    timestamps: true, // saber quando os registros foram criados por data
+    underscored: true, // tabelas: telefone_usuario
     undescoredAll: true,
   },
 };
