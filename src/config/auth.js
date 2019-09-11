d760a95021d9d776c51bacfa88626e323e7855a7
quixote15 <santos2tiago@gmail.com>
2019-09-11 17:55:32 -0300

    Finish authentication

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '54217a3ddd4dba858c14f831f11e98aa',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
+
+import authMiddleware from './app/middlewares/auth';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello ma9' });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
