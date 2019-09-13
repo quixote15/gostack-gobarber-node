312720ce53cd9ecf28c1d14de35a6e28b0975643
quixote15 <santos2tiago@gmail.com>
2019-09-13 15:37:37 -0300

    Add error management tools

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 import express from 'express';
-import routes from './routes';
+import 'express-async-errors';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+
+import sentryConfig from './config/sentry';
+import routes from './routes';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   /**
    * Para cadastrar todos os middlewares
    */
   middlewares() {
+    // The request handler must be the first middleware on the app
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -26,6 +37,14 @@ class App {
    */
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
